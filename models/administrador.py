b177d39f8e5ba5c70e91dbdbfca28cffad2f5ae5
MiichF <paulina.figueroa@alumnos.udg.mx>
2022-04-08 16:11:52 -0500

    validación administrador y contraseña admin

--- models/administrador.py
+++ models/administrador.py
@@ -11,18 +11,16 @@ sys.path.append(path)
   
 
 from db import *
-#funcion para realizar consulta a la base de datos
-def login_admin(user_name , password): 
+
+def login_admin(user_name, password): 
    #establecemos un cursor             
     cur = db.cursor()
     #sentencia sql para hacer la consulta
-    sql = "SELECT * FROM administrador WHERE contrasena = SHA2(%s,256) and nombre_usuario = %s "
-    cur = db.cursor()
-    cur.execute(sql, (user_name, password))
+    sql = "SELECT * FROM administrador WHERE nombre_usuario=%s AND contrasena=SHA2(%s,256)"
+    cur.execute(sql,(user_name,password))
     #obtenemos los registros
     admin = cur.fetchall()
     #cerramos la conexion
     cur.close()
     #devolvemos los registros
     return admin
-
