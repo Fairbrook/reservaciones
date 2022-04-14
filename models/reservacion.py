a189bb4e7f04fe1af413d23481be486c37d82221
FernandoQuantum <ferrussman@gmail.com>
2022-04-14 09:18:05 -0500

    Validaciones exitosas
    Validacion de reservacion activa, ultima reservacion hace 24 hrs, fecha pasada y fecha dentro del rango de anticipación completadas

--- models/reservacion.py
+++ models/reservacion.py
@@ -31,41 +31,48 @@ def validar_reservacion(id_cliente, fecha, hora, zona, cupos):
     sql = "SELECT (n_mesas_z{}) FROM restaurante where id_restaurante = {}".format(zona_db, id_restaurante)
     cursor.execute(sql)    
     limite_cupos = cursor.fetchone()[0]
-    cursor.close()
     
-    cursor = db.cursor()
-    sql_validacion1 = "SELECT STR_TO_DATE('{}', '%d/%m/%y %H:%i') < now()".format(fecha_hora_db)
+
+    #1.- Buscamos en la bd si existe alguna reservacion ACTIVA de este cliente, en cuanto encuentra una, termina y devuelve resultado
+    sql_validacion1 = "SELECT COUNT(*) from reservacion where id_cliente = {} and estatus = 'A' Limit 1".format(id_cliente)
     cursor.execute(sql_validacion1)
     no_valido_1 = cursor.fetchone()[0]
-    cursor.close()
-    
-    #Primero validamos que la reservación sea en una fecha futura
-    if no_valido_1:
-        return False, "La fecha y hora de esta reservación ya pasaron"
+
+    if no_valido_1 == 1: #Si existió una reservación ACTIVA, no se acepta
+        return False, "Este usuario ya cuenta con una reservación activa"
     else:
-        
-        cursor = db.cursor()
-        sql_validacion2 = "SELECT STR_TO_DATE('{}', '%d/%m/%y %H:%i') > Date_add(now(), interval 8 day)".format(fecha_hora_db)
+        #Ahora buscaremos que la última reservación terminada (ya sea TERMINADA o ASISTIDA) haya sido hace
+        #más de 24 hrs, de otra manera no permitimos reservación
+        sql_ultima_reservacion = '''Select MAX(hora_fecha) from reservacion where 
+                                    id_cliente = {} and estatus = 'S' or estatus = 'T' '''.format(id_cliente)
+        cursor.execute(sql_ultima_reservacion)
+        fecha_ultima_reserva = cursor.fetchone()[0]
+
+        sql_validacion2 = '''SELECT '{}' > date_sub(now(), interval 24 hour)'''.format(fecha_ultima_reserva)
         cursor.execute(sql_validacion2)
         no_valido_2 = cursor.fetchone()[0]
-        cursor.close()
 
-        #Ahora comprobamos que la reserva no esté más lejana que un año
         if no_valido_2:
-            return False, "La anticipación máxima de una reservación es de una semana"  
+            return False, '''Su última reservación fue hace menos de 24 horas\n
+            Vuelva una vez se haya cumplido el plazo mínimo de espera\nSu última reservación: {}'''.format(fecha_ultima_reserva)
         else:
-
-            cursor = db.cursor()
-
-            #Buscamos en la bd si existe alguna reservacion ACTIVA de este cliente, en cuanto encuentra una, termina y devuelve resultado
-            sql_validacion3 = "SELECT COUNT(*) from reservacion where id_cliente = {} and estatus = 'A' Limit 1".format(id_cliente)
+            
+            sql_validacion3 = "SELECT STR_TO_DATE('{}', '%d/%m/%y %H:%i') < now()".format(fecha_hora_db)
             cursor.execute(sql_validacion3)
             no_valido_3 = cursor.fetchone()[0]
-            cursor.close()
 
-            #Ahora comprobamos que este usuario no cuente con ninguna otra reservacion ACTIVA
-            if no_valido_3 == 1: #Si existió una reservación ACTIVA, no se acepta
-                return False, "Este usuario ya cuenta con una reservación activa"
+            #Comprobamos que la reservacion sea en una fecha/hora futura
+            if no_valido_3:
+                return False, "La fecha y hora de esta reservación ya pasaron"
             else:
+                
+                sql_validacion4 = "SELECT STR_TO_DATE('{}', '%d/%m/%y %H:%i') > Date_add(now(), interval 8 day)".format(fecha_hora_db)
+                cursor.execute(sql_validacion4)
+                no_valido_4 = cursor.fetchone()[0]
+                cursor.close()
 
-                pass
+                #Ahora comprobamos que la reserva no esté más lejana que una semana
+                if no_valido_4:
+                    return False, "La anticipación máxima de una reservación es de una semana"  
+                else:
+                    return True, "Todo bien"                    
